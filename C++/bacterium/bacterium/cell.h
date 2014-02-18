cce193d4b591a7a921c6068eca5076fba83cce62
Volodymyr Gamula <v.gamula@gmail.com>
2014-02-18 12:01:13 +0200

    Initial v2

--- C++/bacterium/bacterium/cell.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-ref struct cell
-{
-	int x,y;
-	bool enabled;
-};
-
--- C++/bacterium/bacterium/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// bacterium.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
